fd9dcadfe5e6ba22d3797c6024ceb8e62a495b9d
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2018-11-08 20:30:58 +0100

    basic tree structure

--- src/app/components/tree-structure/tree-structure.component.ts
+++ src/app/components/tree-structure/tree-structure.component.ts
@@ -1,17 +1,49 @@
 import { Component, Input } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'tree-structure',
   templateUrl: './tree-structure.component.html',
-  styleUrls: ['./tree-structure.componentt.sass']
+  styleUrls: ['./tree-structure.component.sass'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        backgroundColor: 'green',
+        transform: 'scale(1)'
+      })),
+      state('close', style({
+        backgroundColor: 'red',
+        transform: 'scale(1.5)'
+      })),
+      transition('*=>state1', animate('300ms')),
+      transition('*=>state2', animate('500ms'))
+    ])
+  ]
 })
 
 export class TreeStructureComponent {
   @Input() data;
   @Input() steps;
-  @Input() color;
+  @Input() day;
+  @Input() currentState;
 
   constructor() { }
 
+  selectThisStep(task, i) {
+    //if it's not the firs element
+    //and the preceding element has been done
+    //then select it and start animation
+    console.log(this.steps[i-1]);
+    if (i > 0 && this.steps[i-1].done === true) {
+      task.selected = true;
+      changeState("state2");
+    }
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+  }
 
 }
--- src/app/views/calendar/calendar.component.ts
+++ src/app/views/calendar/calendar.component.ts
@@ -6,6 +6,79 @@ import { Component, ViewChild } from '@angular/core';
 })
 export class CalendarComponent {
 
+  today="mon";
+  data = [
+    {
+      name: "School",
+      img: "image",
+      done: true,
+      selected: false,
+      /* steps: [
+        {
+          name: "Bus",
+          img: "image", */
+          /* steps: [
+            {
+              name: "Go to the stop",
+              img: "image",
+            },
+            {
+              name: "Wait 4 stops",
+              img: "image",
+            },
+            {
+              name: "Enter school",
+              img: "image",
+              steps: [
+                {
+                  name: "Math",
+                  img: "image",
+                  steps: [
+                    {
+                      name: "Grab the book",
+                      img: "image",
+                    },
+                    {
+                      name: "Go to room 102",
+                      img: "image",
+                    },
+                  ],
+                },
+                {
+                  name: "Eng",
+                  img: "image",
+                  steps: [
+                    {
+                      name: "Grab the book",
+                      img: "image",
+                    },
+                    {
+                      name: "Go to room 423",
+                      img: "image",
+                    },
+                  ],
+                }
+              ],
+            }
+          ], */
+     /*    }
+      ], */
+    },
+    {
+      name: "step 2",
+      img: "image",
+      steps: [],
+      done: false,
+      selected: false,
+    },
+    {
+      name: "step 3",
+      img: "image",
+      steps: [],
+      done: false,
+      selected: false,
+    }
+  ]
 
   constructor(
   ) {
