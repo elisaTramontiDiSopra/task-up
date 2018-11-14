1be21ada5b231c78f1d49c016ed0ef015d10b782
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2018-11-14 21:19:03 +0100

    done subtasks change parent-task class

--- src/app/components/tree-structure/tree-structure.component.ts
+++ src/app/components/tree-structure/tree-structure.component.ts
@@ -1,46 +1,91 @@
 import { Component, Input } from "@angular/core";
-import { trigger, state, style, animate, transition, keyframes, query, stagger} from "@angular/animations";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes,
+  query,
+  stagger
+} from "@angular/animations";
+import { timingSafeEqual } from "crypto";
 
 @Component({
   selector: "tree-structure",
   templateUrl: "./tree-structure.component.html",
   styleUrls: ["./tree-structure.component.sass"],
   animations: [
-    trigger('slideInLeft', [
-      transition('* => *', [
-        query(':enter', style({ opacity: 0 }), {optional: true}),
-        query(':enter', stagger('120ms', [
-          animate('0.5s ease-in', keyframes([
-            style({opacity: 0, transform: 'translateX(-25%)', offset: 0}),
-            style({opacity: .5, transform: 'translateX(15px)', offset: 0.3}),
-            style({opacity: 1, transform: 'translateX(0)', offset: 1.0}),
-          ]))]), {optional: true})
+    trigger("slideInLeft", [
+      transition("* => *", [
+        query(":enter", style({ opacity: 0 }), { optional: true }),
+        query(
+          ":enter",
+          stagger("120ms", [
+            animate(
+              "0.5s ease-in",
+              keyframes([
+                style({ opacity: 0, transform: "translateX(-25%)", offset: 0 }),
+                style({
+                  opacity: 0.5,
+                  transform: "translateX(15px)",
+                  offset: 0.3
+                }),
+                style({ opacity: 1, transform: "translateX(0)", offset: 1.0 })
+              ])
+            )
+          ]),
+          { optional: true }
+        )
       ])
     ]),
-    trigger('slideInDown', [
-      transition('* => *', [
-        query(':enter', style({ opacity: 0 }), {optional: true}),
-        query(':enter', stagger('120ms', [
-          animate('0.5s ease-in', keyframes([
-            style({opacity: 0, transform: 'translateY(-25%)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(15px)', offset: 0.3}),
-            style({opacity: 1, transform: 'translateY(0)', offset: 1.0}),
-        ]))]), {optional: true}),
-        query(':leave', stagger('-120ms', [
-          animate('0.2s ease-in', keyframes([
-            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
-            style({opacity: 0, transform: 'translateY(-25%)', offset: 1.0}),
-        ]))]), {optional: true})
+    trigger("slideInDown", [
+      transition("* => *", [
+        query(":enter", style({ opacity: 0 }), { optional: true }),
+        query(
+          ":enter",
+          stagger("120ms", [
+            animate(
+              "0.5s ease-in",
+              keyframes([
+                style({ opacity: 0, transform: "translateY(-25%)", offset: 0 }),
+                style({
+                  opacity: 0.5,
+                  transform: "translateY(15px)",
+                  offset: 0.3
+                }),
+                style({ opacity: 1, transform: "translateY(0)", offset: 1.0 })
+              ])
+            )
+          ]),
+          { optional: true }
+        ),
+        query(
+          ":leave",
+          stagger("-120ms", [
+            animate(
+              "0.2s ease-in",
+              keyframes([
+                style({ opacity: 1, transform: "translateY(0)", offset: 0 }),
+                style({
+                  opacity: 0,
+                  transform: "translateY(-25%)",
+                  offset: 1.0
+                })
+              ])
+            )
+          ]),
+          { optional: true }
+        )
       ])
-    ]),
+    ])
   ]
 })
-
 export class TreeStructureComponent {
   @Input() direction;
   @Input() steps;
+  @Input() parentTask;
   @Input() day;
-  @Input() currentState;
 
   constructor() {}
 
@@ -48,37 +93,45 @@ export class TreeStructureComponent {
     //if the parent task is clicked make subtask visible
     task.stepsVisible = !task.stepsVisible;
 
-
-    //if it's not the firs element
+    //if it's not the first element
     //and the preceding element has been done
     //then select it and start animation
     console.log(task);
-    if (i > 0 && this.steps[i - 1].done === true) {
-      task.selected = true;
-      //changeState("state2");
-    };
-    if (i = 0) {
+    if ((i > 0 && this.steps[i - 1].done === true) || i === 0) {
       task.selected = true;
     }
-
   }
 
+  done(task, i) {
+    task.done = true;
+    //check if this is the last subtask and eventually mark the parent task as completed
+    if (i+1 === this.steps.length) {
+      this.parentTask.completed = true;
+      console.log("completed");
+      console.log(this.parentTask);
+      console.log(task);
+    }
+    console.log(task);
+  }
 
+  //mark a task as SKIPPED
+  skipped() {}
+
+  /* NOT USED IF I KEEP THE VERTICAL SHOWING FOR ALL SUBGROUPS
   horOrVert(i) {
     if (this.direction === "vert") {
       return 'horiz'
     } else {
       return 'vert'
     }
-  }
+  } */
 
   ngOnInit(): void {
-    //add stepVisible: false to each element so that it can become visible only when it's clicked
-    this.steps.map((step) => {
+    //this.steps = this.parentTask.steps;
+    this.steps.map(step => {
       step = {
         stepVisible: false
-      }
-    })
-
+      };
+    });
   }
 }
--- src/app/views/calendar/calendar.component.ts
+++ src/app/views/calendar/calendar.component.ts
@@ -1,64 +1,74 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
 
 @Component({
-  templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.sass']
+  templateUrl: "./calendar.component.html",
+  styleUrls: ["./calendar.component.sass"]
 })
 export class CalendarComponent {
-
-  today="mon";
+  today = "mon";
   data = [
     {
       name: "School",
       img: "image",
       done: false,
-      selected: false,
+      skipped: false,
+      completed: false,
       steps: [
         {
           name: "Bus stop",
           img: "image",
           done: false,
+          skipped: false,
+          completed: false
         },
         {
           name: "Bus",
           img: "image",
           done: false,
+          skipped: false,
+          completed: false,
           steps: [
             {
               name: "Go to the stop",
               img: "image",
+              done: false,
+              skipped: false
             },
             {
               name: "Wait 4 bus",
               img: "image",
+              done: false,
+              skipped: false
             }
-          ],
+          ]
         },
         {
           name: "Sit on bus",
           img: "image",
-        },
-      ],
+          done: false,
+          skipped: false
+        }
+      ]
     },
     {
       name: "step 2",
       img: "image",
       steps: [],
       done: false,
+      skipped: false,
       selected: false,
+      completed: false
     },
     {
       name: "step 3",
       img: "image",
       steps: [],
       done: false,
+      skipped: false,
       selected: false,
+      completed: false
     }
-  ]
+  ];
 
-  constructor(
-  ) {
-  }
-
-
+  constructor() {}
 }
