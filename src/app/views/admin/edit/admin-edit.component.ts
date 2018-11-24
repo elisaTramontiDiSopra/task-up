456293fc4095bd84c20486812d6cbccf4e883f89
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2018-11-24 12:20:53 +0100

    Fix some basic styling admin pages

--- src/app/views/admin/edit/admin-edit.component.ts
+++ src/app/views/admin/edit/admin-edit.component.ts
@@ -5,10 +5,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-edit.component.sass'],
 })
 export class AdminEditComponent {
-
+  selectedTab = 'mon';
+  mondayTasks=[
+    {name: "step1", img: "image", steps: [], done: false, skipped: false, selected: false, completed: false },
+    {name: "step2", img: "image", steps: [], done: false, skipped: false, selected: false, completed: false },
+    {name: "step3", img: "image", steps: [], done: false, skipped: false, selected: false, completed: false },
+    {name: "step4", img: "image", steps: [], done: false, skipped: false, selected: false, completed: false },
+  ];
 
   constructor() { }
 
-
+  move(task, i, direction) {
+    let tempTask = task;
+    this.mondayTasks.splice(i, 1); //remove task
+    if(direction === "up") {
+      this.mondayTasks.splice(i-1, 0, tempTask);
+    } else {
+      this.mondayTasks.splice(i+1, 0, tempTask);
+    }
+  }
 
 }
